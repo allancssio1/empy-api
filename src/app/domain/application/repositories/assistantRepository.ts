1f150ee5c1f20d592b65bf74aee42325078c3c96
Allan Cassio <allan.cassio1@gmail.com>
2024-03-03 14:14:54 -0300

    create method findByEmail

--- src/app/domain/application/repositories/assistantRepository.ts
+++ src/app/domain/application/repositories/assistantRepository.ts
@@ -3,6 +3,7 @@ import { Assistant } from '../../enterprise/entities/Assistant'
 export interface AssistantRepository {
   create(assistant: Assistant): Promise<Assistant>
   update(assistant: Assistant): Promise<Assistant>
+  findByEmail(email: string): Promise<Assistant | null>
   findMany(): Promise<Assistant[] | []>
   delete(assistant: Assistant): Promise<void>
 }
