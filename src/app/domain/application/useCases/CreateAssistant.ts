95fc34e9a034c0d8477c4e307a5612906bedecc8
Allan Cassio <allan.cassio1@gmail.com>
2024-03-03 14:15:17 -0300

    create use case create Assistant

---
+++ src/app/domain/application/useCases/CreateAssistant.ts
@@ -0,0 +1,37 @@
+import { Assistant } from '../../enterprise/entities/Assistant'
+import { AssistantRepository } from '../repositories/assistantRepository'
+
+interface CreateAssistantUseCaseProps {
+  name: string
+  email: string
+  phone: string
+}
+
+interface CreateAssistantUseCaseResponse {
+  assistant: Assistant
+}
+
+export class CreateAssistantUseCase {
+  constructor(private assistantRepository: AssistantRepository) {}
+
+  async handle({
+    name,
+    email,
+    phone,
+  }: CreateAssistantUseCaseProps): Promise<CreateAssistantUseCaseResponse> {
+    const assistantAlreadyExists =
+      await this.assistantRepository.findByEmail(email)
+
+    if (assistantAlreadyExists) throw new Error('Assistant Already Exists')
+
+    const assistant = Assistant.create({
+      name,
+      email,
+      phone: phone.replace(/\D/g, ''),
+    })
+
+    await this.assistantRepository.create(assistant)
+
+    return { assistant }
+  }
+}
---
+++ src/app/domain/application/useCases/CreateAssitant.spec.ts
@@ -0,0 +1,37 @@
+import { makeAssistant } from '#/factories/makeAssitant'
+import { AssistantsInMemoryRepository } from '#/repositories/AssistantsInMemoryRepository'
+import { CreateAssistantUseCase } from './CreateAssistant'
+
+let assistantRepository: AssistantsInMemoryRepository
+let sut: CreateAssistantUseCase
+
+describe('Create Assistant', () => {
+  beforeEach(() => {
+    assistantRepository = new AssistantsInMemoryRepository()
+    sut = new CreateAssistantUseCase(assistantRepository)
+  })
+
+  it('It must be possible to register an assistant.', async () => {
+    const { assistant } = await sut.handle({
+      name: 'Assistant-1',
+      email: 'assistant@email.com',
+      phone: '9989999999',
+    })
+
+    expect(assistant.email).toEqual('assistant@email.com')
+  })
+
+  it('It should not be possible to register an assistant with an existing email address.', async () => {
+    await assistantRepository.create(
+      makeAssistant({ email: 'assistant@email.com' }),
+    )
+    expect(
+      async () =>
+        await sut.handle({
+          name: 'Assistant-1',
+          email: 'assistant@email.com',
+          phone: '9989999999',
+        }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
