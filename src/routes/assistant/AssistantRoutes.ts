f2b16e600e4c424e72d992b1c8356ca1a3dc3560
Allan Cassio <allan.cassio1@gmail.com>
2024-03-06 07:02:03 -0300

    create route create and edit assistant

--- src/routes/assistant/AssistantRoutes.ts
+++ src/routes/assistant/AssistantRoutes.ts
@@ -1,8 +1,12 @@
+import { createAssistantController } from '@/app/http/controllers/assistents/createAssistant'
+import { editAssistantController } from '@/app/http/controllers/assistents/editAssistant'
 import { listAssistantsController } from '@/app/http/controllers/assistents/listAssistants'
 import { Router } from 'express'
 
 const assistantsRoutes = Router()
 
 assistantsRoutes.get('/list', listAssistantsController)
+assistantsRoutes.post('/create', createAssistantController)
+assistantsRoutes.post('/edit/:id', editAssistantController)
 
 export { assistantsRoutes }
