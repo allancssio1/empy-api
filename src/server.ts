600edcaf3af21a800e63e7612ffd9f3a46b1d552
Allan Cassio <allan.cassio1@gmail.com>
2024-03-06 19:09:48 -0300

    implement cors

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,11 @@ import { router } from './routes'
 import { env } from '@/env'
 import swaggerUI from 'swagger-ui-express'
 import docsFile from './swagger.json'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(docsFile))
 app.use(router)
